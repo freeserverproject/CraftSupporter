dc8d74b16dc762aad792a747edc1d2ca87a723ce
神保 恒介 <81564943+kosukej@users.noreply.github.com>
2022-11-14 20:20:34 +0900

    [Add] ItemStackAPIの実装

---
+++ src/main/java/pro/freeserver/plugin/alphakun/craftsupporter/classes/ItemStackAPI.kt
@@ -0,0 +1,33 @@
+package pro.freeserver.plugin.alphakun.craftsupporter.classes
+
+import org.bukkit.Material
+import org.bukkit.enchantments.Enchantment
+import org.bukkit.inventory.ItemStack
+
+class ItemStackAPI(material: Material, amount: Int, itemName: String, lore: List<String>, enchant: List<Enchantment>, customModelData: Int) {
+    var material: Material = Material.AIR
+    var amount: Int = 0
+    var itemName: String = ""
+    var lore: List<String> = listOf()
+    var enchant: List<Enchantment> = listOf()
+    var customModelData: Int = 0
+
+    init {
+        this.material = material
+        this.amount = amount
+        this.itemName = itemName
+        this.lore = lore
+        this.enchant = enchant
+        this.customModelData = customModelData
+    }
+
+    fun getItemStack(): ItemStack {
+        val itemStack = ItemStack(material, amount)
+        val itemMeta = itemStack.itemMeta
+        itemMeta.displayName()
+        itemMeta.setLore(lore)
+        itemMeta.setCustomModelData(customModelData)
+        itemStack.itemMeta = itemMeta
+        return itemStack
+    }
+}
