c1995423a4df5c3a1df57d7978fc94fdd4b5f18b
Alpha9n <42644369+Alpha9n@users.noreply.github.com>
2024-08-19 06:15:23 +0900

    add: コマンド初期化ロジックを追加

--- src/main/java/pro/freeserver/plugin/alphakun/craftsupporter/CraftSupporter.kt
+++ src/main/java/pro/freeserver/plugin/alphakun/craftsupporter/CraftSupporter.kt
@@ -1,12 +1,9 @@
 package pro.freeserver.plugin.alphakun.craftsupporter
 
-import org.bukkit.Bukkit.recipeIterator
-import org.bukkit.Material
+import io.papermc.paper.plugin.lifecycle.event.types.LifecycleEvents
 import org.bukkit.NamespacedKey
-import org.bukkit.inventory.ItemStack
-import org.bukkit.inventory.Recipe
-import org.bukkit.inventory.ShapedRecipe
 import org.bukkit.plugin.java.JavaPlugin
+import pro.freeserver.plugin.alphakun.craftsupporter.commands.CraftCommand
 import pro.freeserver.plugin.alphakun.craftsupporter.init.LoadRecipe
 
 class CraftSupporter : JavaPlugin() {
@@ -15,6 +12,7 @@ class CraftSupporter : JavaPlugin() {
         plugin = this
         this.saveDefaultConfig()
         LoadRecipe().loadRecipe()
+        this.registerCommands()
     }
 
     override fun onDisable() {
@@ -27,10 +25,22 @@ class CraftSupporter : JavaPlugin() {
         var recipes: MutableList<NamespacedKey> = mutableListOf()
     }
 
+    private fun registerCommands() {
+        val commandList: MutableList<CraftCommand> = mutableListOf(pro.freeserver.plugin.alphakun.craftsupporter.commands.recipe.Recipe()) // コマンドリスト
+        val manager = this.lifecycleManager
+        manager.registerEventHandler(LifecycleEvents.COMMANDS) { event ->
+            val commands = event.registrar()
+            commandList.forEach {
+                println("Command ${it.name} is registered")
+                commands.register(it.name, it.description, it)
+            }
+        }
+    }
+
     private fun removeRecipe() {
         for (r in recipes) {
             if (server.removeRecipe(r)) {
-                println("Recipe: " + r.key + "is now removed!!")
+                println("Recipe: ${r.key} is now removed!!")
             }
         }
     }
