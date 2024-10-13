105caf0196e8077b77a593f032acd1b3b366a84a
Alpha9n <42644369+Alpha9n@users.noreply.github.com>
2024-10-13 12:52:48 +0900

    fix: 不要なコマンドの削除

--- src/main/java/pro/freeserver/plugin/alphakun/craftsupporter/CraftSupporter.kt
+++ src/main/java/pro/freeserver/plugin/alphakun/craftsupporter/CraftSupporter.kt
@@ -1,9 +1,7 @@
 package pro.freeserver.plugin.alphakun.craftsupporter
 
-import io.papermc.paper.plugin.lifecycle.event.types.LifecycleEvents
 import org.bukkit.NamespacedKey
 import org.bukkit.plugin.java.JavaPlugin
-import pro.freeserver.plugin.alphakun.craftsupporter.commands.CraftCommand
 import pro.freeserver.plugin.alphakun.craftsupporter.init.LoadRecipe
 
 class CraftSupporter : JavaPlugin() {
@@ -12,7 +10,6 @@ class CraftSupporter : JavaPlugin() {
         plugin = this
         this.saveDefaultConfig()
         LoadRecipe().loadRecipe()
-        this.registerCommands()
     }
 
     override fun onDisable() {
@@ -23,18 +20,6 @@ class CraftSupporter : JavaPlugin() {
     companion object {
         lateinit var plugin: CraftSupporter
         var recipes: MutableList<NamespacedKey> = mutableListOf()
-    }
-
-    private fun registerCommands() {
-        val commandList: MutableList<CraftCommand> = mutableListOf(pro.freeserver.plugin.alphakun.craftsupporter.commands.recipe.Recipe()) // コマンドリスト
-        val manager = this.lifecycleManager
-        manager.registerEventHandler(LifecycleEvents.COMMANDS) { event ->
-            val commands = event.registrar()
-            commandList.forEach {
-                println("Command ${it.name} is registered")
-                commands.register(it.name, it.description, it)
-            }
-        }
     }
 
     private fun removeRecipe() {
